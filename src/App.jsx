80e2e520021a4c4a036df0b8c1ca56f757f5db1a
alielghazoly1 <alielghazoly769@gmail.com>
2025-12-04 00:20:21 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,65 @@
 import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+// react
+import { useEffect, useState } from 'react';
 // materaial UI components
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import CloudIcon from '@mui/icons-material/Cloud';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+// external libraries
+import axios from 'axios';
+import moment from 'moment/min/moment-with-locales';
+moment.locale("ar");
 const theme = createTheme({
   typography: {
     fontFamily: ['IBM'],
   },
 });
+let cancelAxios = null;
 function App() {
+  let [dateAndTime, setDateAndTime] = useState('');
+  let [temp, setTemp] = useState({
+    number: null,
+    description: '',
+    min: null,
+    max: null,
+    icon: null,
+  });
+  useEffect(() => {
+    setDateAndTime(moment().format("dddd - D MMMM YYYY - h:mm:ss a"));
+    axios
+      .get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=30.033333&lon=31.233334&appid=38c8f743dc9dad3b44f625edcbe1f760',
+        {
+          cancelToken: new axios.CancelToken((c) => {
+            cancelAxios = c;
+          }),
+        }
+      )
+      .then(function (response) {
+        // handle success
+        let responseTemp = Math.round(response.data.main.temp - 272.15);
+        let min = Math.round(response.data.main.temp_min - 272.15);
+        let max = Math.round(response.data.main.temp_max - 272.15);
+        let description = response.data.weather[0].description;
+        let responseIcon = response.data.weather[0].icon;
+        setTemp({
+          number: responseTemp,
+          min: min,
+          max: max,
+          description: description,
+          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+    return () => {
+      cancelAxios();
+    };
+  }, []);
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -61,7 +110,7 @@ function App() {
                       gutterBottom
                       style={{ marginRight: '20px' }}
                     >
-                      الاثنين 10-1-2026
+                      {dateAndTime}
                     </Typography>
                   </div>
                   {/* =====city & time==== */}
@@ -73,16 +122,22 @@ function App() {
                   >
                     <div>
                       {/* temp */}
-                      <div>
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}
+                      >
                         <Typography
                           variant="h1"
                           gutterBottom
                           style={{ textAlign: 'right' }}
                         >
-                          38
+                          {temp.number}
                         </Typography>
                         {/* temp IMAGE */}
-                        {/* ============= */}
+                        <img src={temp.icon} alt="" />
                         {/* temp IMAGE */}
                       </div>
                       <Typography
@@ -90,7 +145,7 @@ function App() {
                         gutterBottom
                         style={{ textAlign: 'right' }}
                       >
-                        broken cloude
+                        {temp.description}
                       </Typography>
                       {/* min & max */}
                       <div
@@ -100,8 +155,8 @@ function App() {
                           alignItems: 'center',
                         }}
                       >
-                        <h5>الصغري:34</h5>
-                        <h5>الكبري:34</h5>
+                        <h5> الصغري: {temp.min} </h5>
+                        <h5> الكبري: {temp.max} </h5>
                       </div>
                       {/*==== temp ====*/}
                     </div>
